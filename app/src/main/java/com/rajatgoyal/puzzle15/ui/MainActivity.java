fb6eb11162f070ae665a48e8c37ef0962a0f41a1
Rajat Goyal <rajatgoyal715@gmail.com>
2019-02-04 11:02:02 +0530

    Display username from players client

--- app/src/main/java/com/rajatgoyal/puzzle15/ui/MainActivity.java
+++ app/src/main/java/com/rajatgoyal/puzzle15/ui/MainActivity.java
@@ -19,6 +19,12 @@ import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.games.AchievementsClient;
+import com.google.android.gms.games.EventsClient;
+import com.google.android.gms.games.Games;
+import com.google.android.gms.games.LeaderboardsClient;
+import com.google.android.gms.games.Player;
+import com.google.android.gms.games.PlayersClient;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.rajatgoyal.puzzle15.R;
@@ -46,6 +52,11 @@ public class MainActivity extends AppCompatActivity {
 
     private Button signInSignOutButton;
     private boolean signedIn = false;
+
+    private AchievementsClient mAchievementsClient;
+    private LeaderboardsClient mLeaderboardsClient;
+    private EventsClient mEventsClient;
+    private PlayersClient mPlayersClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -144,20 +155,44 @@ public class MainActivity extends AppCompatActivity {
 
     private void onConnected(GoogleSignInAccount googleSignInAccount) {
         Timber.d("onConnected(): connected to Google APIs");
-        if (signedInAccount != googleSignInAccount) {
-
-            signedInAccount = googleSignInAccount;
-            Toast.makeText(this, signedInAccount.getEmail(), Toast.LENGTH_SHORT).show();
-            Timber.d(signedInAccount.toString());
-        }
         signedIn = true;
         updateSignInSignOutButton();
+
+        if (signedInAccount != googleSignInAccount) {
+            signedInAccount = googleSignInAccount;
+        }
+
+        mAchievementsClient = Games.getAchievementsClient(this, googleSignInAccount);
+        mLeaderboardsClient = Games.getLeaderboardsClient(this, googleSignInAccount);
+        mEventsClient = Games.getEventsClient(this, googleSignInAccount);
+        mPlayersClient = Games.getPlayersClient(this, googleSignInAccount);
+
+        // Set the greeting appropriately on main menu
+        mPlayersClient.getCurrentPlayer()
+                .addOnCompleteListener(new OnCompleteListener<Player>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Player> task) {
+                        String displayName;
+                        if (task.isSuccessful()) {
+                            displayName = task.getResult().getDisplayName();
+                        } else {
+                            Exception e = task.getException();
+                            displayName = "???";
+                        }
+                        Toast.makeText(MainActivity.this, displayName, Toast.LENGTH_SHORT).show();
+                    }
+                });
     }
 
     public void onDisconnected() {
         Timber.d("onDisconnected()");
         signedIn = false;
         updateSignInSignOutButton();
+
+        mAchievementsClient = null;
+        mLeaderboardsClient = null;
+        mEventsClient = null;
+        mPlayersClient = null;
     }
 
     public void updateSignInSignOutButton() {
