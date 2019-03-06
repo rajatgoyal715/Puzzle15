1928eed2c2cdc97861b05cc7c8da7a7340a4fcc0
rish971 <rishabhsingh971@gmail.com>
2019-03-06 23:28:04 +0530

    set resumeGame flag to false on starting a new game

--- app/src/main/java/com/rajatgoyal/puzzle15/ui/MainActivity.java
+++ app/src/main/java/com/rajatgoyal/puzzle15/ui/MainActivity.java
@@ -253,6 +253,7 @@ public class MainActivity extends AppCompatActivity {
                     DialogInterface.OnClickListener yesListener = new DialogInterface.OnClickListener() {
                         @Override
                         public void onClick(DialogInterface dialog, int which) {
+                            SharedPref.setResumeGame(false);
                             startGameActivity();
                         }
                     };
