f5cdd34b8d50de41a33fa51e1af7bc41135fdbb5
Rajat Goyal <rajatgoyal715@gmail.com>
2019-04-19 19:19:30 +0530

    Calculate score from moves and time

--- app/src/main/java/com/rajatgoyal/puzzle15/model/HighScore.java
+++ app/src/main/java/com/rajatgoyal/puzzle15/model/HighScore.java
@@ -7,10 +7,12 @@ package com.rajatgoyal.puzzle15.model;
 public class HighScore {
     private int moves;
     private Time time;
+    private int score;
 
     public HighScore(int moves, Time time) {
         this.moves = moves;
         this.time = time;
+        this.score = (int)((double)100 * Math.exp(- (double) (moves + time.toSeconds()) / (double) 100));
     }
 
     public int getMoves() {
@@ -19,5 +21,9 @@ public class HighScore {
 
     public Time getTime() {
         return time;
+    }
+
+    public int getScore() {
+        return score;
     }
 }
